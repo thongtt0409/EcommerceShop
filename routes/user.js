77269f6224931d43326c793d34b30b718f4b60c2
Thongttdev <thongtt0409@gmail.com>
2022-05-09 23:20:37 +0700

    update user

--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,23 @@
+const { verifyToken, verifyTokenAndAuthorization } = require("./verifytoken");
+const User = require("../models/User");
 const router = require("express").Router();
+
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_PASS).toString();
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
---
+++ routes/verifyToken.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.token;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.SECRET_JWT, (err, user) => {
+      if (err) res.status(401).json("Mã không hợp lệ");
+      req.user = user;
+      next();
+    });
+  } else {
+    return res.status(401).json("Bạn không được xác thực!");
+  }
+};
+
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("Bạn không có đủ phân quyền!");
+    }
+  });
+};
+module.exports = { verifyToken, verifyTokenAndAuthorization };
